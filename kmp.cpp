6f457ddaf66e7157c3e93df6174790d5cd03a84e
cary <cary.zoumvp@gmail.com>
2014-11-15 00:34:49 +0800

    commit kmp algorithm, only cover some simple case, need to be verified

--- kmp.cpp
+++ kmp.cpp
@@ -2,44 +2,52 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 int* create_part_array (const char *pattern, int len) {
-	int i = 0;
-	int j = 0;
-	int k = 0;
+	int i,j,k,m;
 	int* s = (int*)malloc(sizeof(int)*len);
 	s[0] = 0;
-	for (i = 1; i < len; i++ ){
-		k = 0;
-		for ( j = 0; j < (i+1)/2; j ++) {
-			if (pattern[j] == pattern[i-j]){
-				k++;
-			}	
-		}
-		s[i] = k;
-	
-	} 
-	return s;
+	for (i = 1; i < len; i++ ) { //total string compare
+        for (j = 0; j < i; j ++) {
+            m = 0;
+            for ( int k = 0; k <= j; k++ ) { //substring compare
+                if (pattern[k] == pattern[i-j+k]) //character compare
+                    m++; 
+                else {
+                    m = 0;
+                    break;
+                }
+            }
+            if (m > s[i])
+                s[i] = m;
+        } 
+    }
+    return s;
 }
 
 int kmp (const char *src, int srclen, const char *pattern, int patlen) {
-	int i= 0,j = 0,k = 0;
+    int i= 0,j = 0,k = 0;
 
-	if (patlen > srclen)
-		return 0;
-	int *table = create_part_array(pattern, patlen);
-	for (i = 0; i < srclen; i++ ) {
-		for (j = i; j < srclen ; j ++) {
-			if (k == patlen -1) {
-				return i;
-			}
-			if (src[j] == pattern[k]) {
-				k++;
-			} else {
-				k = table[k];
-				j--;
+    if (patlen > srclen)
+        return 0;
+    int *table = create_part_array(pattern, patlen);
+    for (i = 0; i < srclen; i++ ) {
+        for (j = i; j < srclen ; j ++) {
+            if (k == patlen -1) {
+                free(table);
+                return i;
+            }
+            if (src[j] == pattern[k]) {
+                k++;
+            } else {
+                if (table[k] > 0) {
+                    k -= table[k];
+                    j--;
+                } else {
+                    k = 0;
+                    break;
+                }
 
-			}
+            }
 		}
 	}
 
@@ -49,8 +57,8 @@ int kmp (const char *src, int srclen, const char *pattern, int patlen) {
 
 int main()
 {
-	const char *src = "aabb ddc c aabbaabbccddee";
-	const char *pattern = "aabbaa";
+	const char *src = "ababaa ababaabababaaabba";
+	const char *pattern = "abababa";
 
 	int pos = kmp(src, strlen(src), pattern, strlen(pattern));
 	printf( "first pos is %d\n", pos);
